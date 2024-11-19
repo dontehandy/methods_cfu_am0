99b896fe9c66da467956b7d94958f4b62b40c9ad
Donte Handy <donte.handy@gmail.com>
2024-11-19 10:42:06 -0500

    show knowledge of built in methods

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -6,22 +6,38 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
+
 "Hello World".downcase
 
 
 "Hello World".include?("Hello")
 
+# the .include? method is called on the string object "Hello World"
+# "Hello" is the substring we are checking for
+# .include? method checks if the substring or "Hello" is present in "Hello World"
+# eval = true because Hello is in Hellow World
 
 "Hello World".end_with?("Hello")
+# the .end_with? method is called on the string object "Hellow World"
+# "Hello" is the substring we check to see if its the ending of main string
+# .end_with? checks if the string ends with a specified string
+# eval = false because the main string ends with "World" not "Hello"
 
 
 "Hello World".end_with?("rld")
+# same as above but only focused on finding rld at ending of the main string
+# eval = true because "hello woRLD" ends with "rld"
+
 
 
 12.even?
+# will check if the integer is even
+# eval/return - true, because its even
 
 
 18.next
+# .next computes the next integer by adding 1 to the number
+# eval/return is 19
 
 
 
@@ -47,6 +63,15 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# declare 2
+number1 = 1
+number2 = 2
+
+#explain impact and return
+number1.even? # will give a true if the value assigned to number1 is true
+number2.next # will add 1 to the variable and should return 3
+
+
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +79,13 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+red_team = ["red1", "red2", "red3"]
+blue_team = ["blue1", "blue2", "blue3"]
+
+# puts red_team.inspect    # To see the array content
+# puts blue_team.inspect
+
+puts red_team.count #total count of elements inside array = 3 NIL?
+puts blue_team.last #last element in array = blue3 NIL?
+
